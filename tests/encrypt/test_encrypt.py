5696e914a0c88a6613c969262fb6e9ad44fd76ee
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-07-06 16:38:39 -0300

    Req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,21 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    "Retorna um TypeError se a key for diferente de int."
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("apenasteste", "1")
+
+    "Retorna um TypeError se a message for diferente de str."
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(234, 1)
+
+    "Se key não for índice positivo válido, retorna a string invertida"
+    assert encrypt_message("marcosadriano", 13) == "onairdasocram"
+
+    "Se key for ímpar"
+    assert encrypt_message("marcos", 3) == "ram_soc"
+
+    "Se key for par"
+    assert encrypt_message("adriano", 4) == "ona_irda"
