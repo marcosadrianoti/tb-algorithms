c35342fab22e4a543fb01bd6f70a24d5f172cd07
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-07-06 14:26:22 -0300

    Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    counter = 0
+    for period in permanence_period:
+        if (
+            period[0] is None
+            or period[1] is None
+            or not isinstance(period[0], int)
+            or not isinstance(period[1], int)
+        ):
+            return None
+        if period[0] <= target_time <= period[1]:
+            counter += 1
+    return counter
