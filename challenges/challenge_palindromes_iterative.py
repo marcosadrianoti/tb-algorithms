f59a6ba05298f5bb8c849246c4c05c5f43c96442
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-07-07 19:52:15 -0300

    Req 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,9 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    for id in range(0, len(word)//2):
+        left_caracter = word[id]
+        rigth_caracter = word[(id + 1) * -1]
+        if left_caracter != rigth_caracter:
+            return False
+    return True
