de37bec0b5cda4d7176ab43c6e455c6f7da1f679
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-07-07 19:08:09 -0300

    Req 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) == 0 or not isinstance(nums[0], int):
+        return False
+    nums.sort()
+    duplicated_number = None
+    for id in range(0, len(nums) - 1):
+        if nums[id] == nums[id + 1] and nums[id] >= 0:
+            duplicated_number = nums[id]
+    if duplicated_number is None:
+        return False
+    return duplicated_number
---
+++ challenges/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+ms) == 0 or not isinstance(nums[0], int):
+        return Fal
